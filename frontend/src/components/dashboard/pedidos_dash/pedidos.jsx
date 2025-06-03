5fd2e154b3669c5eda4dfd138c3685451747e1e4
Gabriel Gallan <gabriel31.gal@gmail.com>
2025-06-03 12:26:03 -0300

    Ajustando frontend

--- frontend/src/components/dashboard/pedidos_dash/pedidos.jsx
+++ frontend/src/components/dashboard/pedidos_dash/pedidos.jsx
@@ -35,7 +35,7 @@ export default function Pedidos() {
     return (
         <div className="pedidos_container">
             <h2 className="titulo">Pedidos</h2>
-            <button onClick={() => setShowForm(true)} className="registrar">Registrar</button>
+            <button onClick={() => setShowForm(true)} className="registrar"><svg className="svgIcon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg></button>
             {showForm ? (
                 <PedidoForm onClose={handleCloseForm}/>
             ) : (
